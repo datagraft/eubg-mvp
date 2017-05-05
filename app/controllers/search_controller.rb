714f908c0b64cb6c2cdcc02bd4d465c6ab373f47
Gencer Erdogan <gencer.erdogan@sintef.no>
2017-05-05 16:57:49 +0200

    The MVP is able to search on companies on Atoka, but there are problems when trying to retrieve information about one specific company.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,18 +14,29 @@ class SearchController < ApplicationController
             else
                 response = searchOCGeneral(params[:search].to_s, "", "")
                 @resultSet = response
+                
+                atokaResponse = searchAtoka(params[:search].to_s, "", "")
+                @atokaResultSet = atokaResponse
             end 
         else
-            @resultSet = {}    
+            @resultSet = {}
+            @atokaResultSet = {}
         end
         
         if params[:companyNumber] && !params[:companyNumber].to_s.empty?
             response = searchOCompanyID(params[:companyNumber].to_s)
-            
             @companyOC = response
         else
             @companyOC = {} 
         end
+        
+        if params[:atokaIds] && !params[:atokaIds].to_s.empty?
+            response = searchAtokaCompanyID(params[:atokaIds].to_s) #for some reason, nothing is fetched... the params[:atokaIds] should contain the id captured from @atokaResultSet (see new.html.erb)
+            @companyAtoka = response 
+        else
+            @companyAtoka = {} 
+        end
+        
     end
     
     def searchOCGeneral(companyName, jurisdictionCode="", pageNumber="")
@@ -48,6 +59,20 @@ class SearchController < ApplicationController
     def searchOCompanyID(companyID)
         url = "https://api.opencorporates.com/v0.4/companies/" + params[:jurisdictionCode].to_s + "/" + companyID
         response = HTTParty.get(url)
+        response.parsed_response
+    end
+    
+    def searchAtoka(companyName, countries="", offset="")
+        atokaToken = ENV['ATOKA_TOKEN']
+        url = "https://api.atoka.io/v2/companies?token=" + atokaToken.to_s + "&packages=*" + "&name=" + companyName
+        response = HTTParty.get(url)
+        response.parsed_response
+    end
+    
+    def searchAtokaCompanyID(companyID) #Something wrong is happening here - just no response at all..
+        atokaToken = ENV['ATOKA_TOKEN'] 
+        url = "https://api.atoka.io/v2/companies?token=" + atokaToken.to_s + "&packages=*" + "&ids=" + companyID
+        response = HTTParty.get(url) 
         response.parsed_response
     end
 
