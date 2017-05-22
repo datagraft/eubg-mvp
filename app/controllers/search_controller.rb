2ccf6f9ac1ba2f76506013f32e4a5b50ca98083a
Gencer Erdogan <gencer.erdogan@sintef.no>
2017-05-22 11:22:41 +0200

    no message

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,26 @@
 class SearchController < ApplicationController
     
     def new
+        newSearch = Search.new({company: params[:search], country: params[:jurisdictionFilter]})   
+        newSearch.execute_search
+        @resultSet = newSearch.searchResult.paginate(:page => params[:page], :per_page => 30)
+        
+        
+        #newResultSet = newSearch.searchResult.paginate(:page => params[:page], :per_page => 30)
+        
+        #if previous_search? 
+        #    @resultSet = @previousSearch.searchResult.paginate(:page => params[:page], :per_page => 30)
+        #else
+        #    @resultSet = newResultSet
+        #    @previousSearch = newSearch
+        #end
     end
     
+    #def previous_search?
+    #    !@previousSearch.nil?
+    #end
+    
     def result
-        @newSearch = Search.new({company: params[:search], country: params[:jurisdictionFilter]})
-        @newSearch.execute_search
-        #@resultSet = newSearch.companies.paginate(:page => params[:page], :per_page => 30)
     end
     
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,7 @@
 class Search 
     require 'will_paginate/array'
     
-    attr_reader :companies
+    attr_reader :searchResult, :company, :country
     
     def initialize(searchString = {})
         @company = searchString[:company]
@@ -18,7 +18,7 @@ class Search
         if company_defined? && !country_defined?
             url = "https://api.atoka.io/v2/companies?token=" + atokaToken.to_s + "&packages=*" + "&name=" + @company.to_s + "&limit=50" + "&offset=0"
             response = HTTParty.get(url)
-            companies = response.parsed_response["items"]
+            @searchResult = response.parsed_response["items"]
             totalItems = response.parsed_response["meta"]["count"].to_i
             
             #If number of items in the result is larger than the response limit, then request the next 50 until total items have been retrieved 
@@ -31,24 +31,48 @@ class Search
                     nextPageURL = "https://api.atoka.io/v2/companies?token=" + atokaToken.to_s + "&packages=*" + "&name=" + @company.to_s + "&limit=50" + "&offset=" + offset.to_s
                     nextPageResponse = HTTParty.get(nextPageURL)
                     nextPageCompanies = nextPageResponse.parsed_response["items"]
-                    companies.concat(nextPageCompanies)
+                    @searchResult.concat(nextPageCompanies)
                     
                     offset += 50
                     currentRequest += 1
                 end
-                companies
+                @searchResult
             else
-                companies
+                @searchResult
             end
     
         elsif company_defined? && country_defined?
-            companies = []
+            @country = "uk" if @country == "gb" #Atoka currently accepts 'it', 'uk', 'ru', or '*' as country value
+            
+            url = "https://api.atoka.io/v2/companies?token=" + atokaToken.to_s + "&packages=*" + "&name=" + @company.to_s + "&countries=" + @country.to_s + "&limit=50" + "&offset=0" 
+            response = HTTParty.get(url)
+            @searchResult = response.parsed_response["items"]
+            totalItems = response.parsed_response["meta"]["count"].to_i
+            
+            if totalItems > 50
+                currentRequest = 0
+                offset = 50
+                additionalRequests = totalItems / 50
+                
+                while currentRequest < additionalRequests do
+                    nextPageURL = "https://api.atoka.io/v2/companies?token=" + atokaToken.to_s + "&packages=*" + "&name=" + @company.to_s + "&countries=" + @country.to_s + "&limit=50" + "&offset=" + offset.to_s
+                    nextPageResponse = HTTParty.get(nextPageURL)
+                    nextPageCompanies = nextPageResponse.parsed_response["items"]
+                    @searchResult.concat(nextPageCompanies)
+                    
+                    offset += 50
+                    currentRequest += 1
+                end
+                @searchResult
+            else
+                @searchResult
+            end
+            
         else
-            companies = []
+            @searchResult = []
         end
             
     end
-
     
     def search_open_corporates
         ocToken = ENV['OC_TOKEN']
@@ -56,48 +80,48 @@ class Search
         if company_defined? && !country_defined?
             url = "https://api.opencorporates.com/v0.4/companies/search?api_token=" + ocToken.to_s + "&q=" + @company.to_s
             response = HTTParty.get(url)
-            @companies = response.parsed_response["results"]["companies"]
+            @searchResult = response.parsed_response["results"]["companies"]
             totalPages = response.parsed_response["results"]["total_pages"].to_i
             
-            if totalPages > 20
-                totalPages = 20
-            end
+            #if totalPages > 20
+            #    totalPages = 20
+            #end
             
             if totalPages > 1
                 (2..totalPages).each do |page|
                     nextPageURL = "https://api.opencorporates.com/v0.4/companies/search?api_token=" + ocToken.to_s + "&q=" + @company.to_s + "&page=#{page}"
                     nextPageResponse = HTTParty.get(nextPageURL)
                     nextPageCompanies = nextPageResponse.parsed_response["results"]["companies"]
-                    @companies.concat(nextPageCompanies)
+                    @searchResult.concat(nextPageCompanies)
                 end
-                @companies
+                @searchResult
             else
-                @companies
+                @searchResult
             end
         elsif company_defined? && country_defined?
             url = "https://api.opencorporates.com/v0.4/companies/search?api_token=" + ocToken.to_s + "&q=" + @company.to_s + "&jurisdiction_code=" + @country.to_s
             response = HTTParty.get(url)
             totalPages = response.parsed_response["results"]["total_pages"].to_i
-            @companies = response.parsed_response["results"]["companies"]
+            @searchResult = response.parsed_response["results"]["companies"]
             
-            if totalPages > 20
-                totalPages = 20
-            end
+            #if totalPages > 20
+            #    totalPages = 20
+            #end
             
             if totalPages > 1
-                (2..totalPages).each do |page|
-                    nextPageURL = "https://api.opencorporates.com/v0.4/companies/search?api_token=" + ocToken.to_s + "&q=" + @company.to_s + "&jurisdiction_code=" + @country.to_s + "&page=#{page}"
-                    nextPageResponse = HTTParty.get(nextPageURL)
-                    nextPageCompanies = nextPageResponse.parsed_response["results"]["companies"]
-                    @companies.concat(nextPageCompanies)
-                end
-                @companies
-            else
-                @companies
-            end
-        else
-            @companies = []
-        end
+                (2..totalPages).each do |page| 
+                    nextPageURL = "https://api.opencorporates.com/v0.4/companies/search?api_token=" + ocToken.to_s + "&q=" + @company.to_s + "&jurisdiction_code=" + @country.to_s + "&page=#{page}" 
+                    nextPageResponse = HTTParty.get(nextPageURL) 
+                    nextPageCompanies = nextPageResponse.parsed_response["results"]["companies"] 
+                    @searchResult.concat(nextPageCompanies) 
+                end 
+                @searchResult 
+            else 
+                @searchResult 
+            end 
+        else 
+            @searchResult = [] 
+        end 
     end 
     
 
